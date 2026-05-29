4a09495e45b3da3401f8a546b6c8b2b49360a81f
omarbarghouth <58629211+omarbarghouth@users.noreply.github.com>
2026-05-29 23:56:50 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -12188,4 +12188,3 @@ with app.app_context():
 if __name__ == '__main__':
     import os
     app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-    host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
